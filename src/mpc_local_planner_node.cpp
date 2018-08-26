4d94b6b5c77f0041adeeb9513161e55b0c4e466c
Erik Örjehag <erik@orjehag.se>
2018-08-26 20:14:22 +0200

    asd

--- src/mpc_local_planner_node.cpp
+++ src/mpc_local_planner_node.cpp
@@ -1,3 +1,9 @@
+
+// states: px, py, heading, steer, v
+// control: steer_w, a
+
+
+
 //////////////////////////////////////////////////////////////////////////
 ////////////////             glider.cxx              /////////////////////
 //////////////////////////////////////////////////////////////////////////
@@ -26,10 +32,12 @@
 
 
 adouble endpoint_cost(adouble* initial_states, adouble* final_states,
-                      adouble* parameters,adouble& t0, adouble& tf,
+                      adouble* parameters, adouble& t0, adouble& tf,
                       adouble* xad, int iphase, Workspace* workspace)
 {
    adouble xf = final_states[CINDEX(1)];
+
+   // error px, py, heading
 
    return -(xf);
 }
@@ -41,6 +49,9 @@ adouble endpoint_cost(adouble* initial_states, adouble* final_states,
 adouble integrand_cost(adouble* states, adouble* controls, adouble* parameters,
                      adouble& time, adouble* xad, int iphase, Workspace* workspace)
 {
+  // steer
+  // delta v, delta steer
+
    return 0.0;
 }
 
@@ -95,32 +106,41 @@ void dae(adouble* derivatives, adouble* path, adouble* states,
 ///////////////////  Define the events function ////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
 
-void events(adouble* e, adouble* initial_states, adouble* final_states,
-            adouble* parameters,adouble& t0, adouble& tf, adouble* xad,
-            int iphase, Workspace* workspace)
-
+void events(
+  adouble* e,
+  adouble* initial_states,
+  adouble* final_states,
+  adouble* parameters,
+  adouble& t0,
+  adouble& tf,
+  adouble* xad,
+  int iphase,
+  Workspace* workspace
+)
 {
+    adouble px_0 	    = initial_states[ CINDEX(1) ];
+    adouble py_0 	    = initial_states[ CINDEX(2) ];
+    adouble heading_0 = initial_states[ CINDEX(3) ];
+    adouble steer_0 	= initial_states[ CINDEX(4) ];
+    adouble v_0 	    = initial_states[ CINDEX(5) ];
 
-    adouble x_i 	= initial_states[ CINDEX(1) ];
-    adouble y_i 	= initial_states[ CINDEX(2) ];
-    adouble vx_i 	= initial_states[ CINDEX(3) ];
-    adouble vy_i 	= initial_states[ CINDEX(4) ];
+    adouble px_f 	    = final_states[ CINDEX(1) ];
+    adouble py_f 	    = final_states[ CINDEX(2) ];
+    adouble heading_f = final_states[ CINDEX(3) ];
+    adouble steer_f 	= final_states[ CINDEX(4) ];
+    adouble v_f 	    = final_states[ CINDEX(5) ];
 
-    adouble x_f 	= final_states[ CINDEX(1) ];
-    adouble y_f		= final_states[ CINDEX(2) ];
-    adouble vx_f	= final_states[ CINDEX(3) ];
-    adouble vy_f 	= final_states[ CINDEX(4) ];
+    e[ CINDEX(1) ] = px_0;
+    e[ CINDEX(2) ] = py_0;
+    e[ CINDEX(3) ] = heading_0;
+    e[ CINDEX(4) ] = steer_0;
+    e[ CINDEX(5) ] = v_0;
 
-    e[ CINDEX(1) ] 	    = x_i;
-    e[ CINDEX(2) ]      = y_i;
-    e[ CINDEX(3) ]      = vx_i;
-    e[ CINDEX(4) ]    	= vy_i;
-
-    e[ CINDEX(5) ]      = y_f;
-    e[ CINDEX(6) ]   	= vx_f;
-    e[ CINDEX(7) ]  	= vy_f;
-
-
+    e[ CINDEX(6) ]  = px_f;
+    e[ CINDEX(7) ]  = py_f;
+    e[ CINDEX(8) ]  = heading_f;
+    e[ CINDEX(9) ]  = steer_f;
+    e[ CINDEX(10) ] = v_f;
 }
 
 
@@ -173,9 +193,9 @@ int main(void)
 /////////   Define phase related information & do level 2 setup  ////////////
 /////////////////////////////////////////////////////////////////////////////
 
-    problem.phases(1).nstates   		= 4;
-    problem.phases(1).ncontrols 		= 1;
-    problem.phases(1).nevents   		= 7;
+    problem.phases(1).nstates   		= 5;
+    problem.phases(1).ncontrols 		= 2;
+    problem.phases(1).nevents   		= 10;
     problem.phases(1).npath     		= 0;
     problem.phases(1).nodes                     = "[20]"; //30 40 50 80
 
@@ -188,24 +208,25 @@ int main(void)
 ///////////////////  Enter problem bounds information //////////////////////
 ////////////////////////////////////////////////////////////////////////////
 
-
-    problem.phases(1).bounds.lower.states = "[0.0      0.0    0.0  -4.0]";
-    problem.phases(1).bounds.upper.states = "[1500.0  1100.0 15.0   4.0]";
-
-
-    problem.phases(1).bounds.lower.controls(1) = 0.0;
-
-    problem.phases(1).bounds.upper.controls(1) = 1.4;
+    //                                         px     py     heading   steer (pi/2)    v
+    problem.phases(1).bounds.lower.states = "[-1e6   -1e6     -1e6       -1.57       -0.1]";
+    problem.phases(1).bounds.upper.states = "[ 1e6    1e6      1e6        1.57        0.1]";
 
 
-    problem.phases(1).bounds.lower.events="[0.0,1000.0,13.2275675,-1.28750052,900.00,13.2275675,-1.28750052 ]";
-    problem.phases(1).bounds.upper.events="[0.0,1000.0,13.2275675,-1.28750052,900.00,13.2275675,-1.28750052 ]";
+    problem.phases(1).bounds.lower.controls(1) = -0.1; // steer_w
+    problem.phases(1).bounds.upper.controls(1) = 0.1;
 
+    problem.phases(1).bounds.lower.controls(2) = -0.1; // a
+    problem.phases(1).bounds.upper.controls(2) = 0.1;
+
+    //                                         px_0    py_0   heading_0   steer_0   v_0    px_f   py_f  heading_f   steer_f  v_f
+    problem.phases(1).bounds.lower.events = "[ 0.0,    0.0,     0.0,        0.0,    0.0,   0.2    0.1      0.0        0.0    0.0]";
+    problem.phases(1).bounds.upper.events = "[ 0.0,    0.0,     0.0,        0.0,    0.0,   0.2    0.1      0.0        0.0    0.0]";
 
     problem.phases(1).bounds.lower.StartTime    = 0.0;
     problem.phases(1).bounds.upper.StartTime    = 0.0;
 
-    problem.phases(1).bounds.lower.EndTime      = 0.1;
+    problem.phases(1).bounds.lower.EndTime      = 0.0;
     problem.phases(1).bounds.upper.EndTime      = 200.0;
 
 
@@ -215,11 +236,11 @@ int main(void)
 ////////////////////////////////////////////////////////////////////////////
 
 
-    problem.integrand_cost 	= &integrand_cost;
-    problem.endpoint_cost 	= &endpoint_cost;
-    problem.dae             	= &dae;
-    problem.events 		= &events;
-    problem.linkages		= &linkages;
+    problem.integrand_cost = &integrand_cost;
+    problem.endpoint_cost  = &endpoint_cost;
+    problem.dae            = &dae;
+    problem.events 		     = &events;
+    problem.linkages		   = &linkages;
 
 ////////////////////////////////////////////////////////////////////////////
 ///////////////////  Define & register initial guess ///////////////////////
@@ -250,8 +271,8 @@ int main(void)
 ////////////////////////////////////////////////////////////////////////////
 
 
-    algorithm.nlp_iter_max                = 1000;
-    algorithm.nlp_tolerance               = 1.e-6;
+    algorithm.nlp_iter_max                = 100; //= 1000;
+    algorithm.nlp_tolerance               = 1.e-3; //= 1.e-6;
     algorithm.nlp_method                  = "IPOPT";
     algorithm.scaling                     = "automatic";
     algorithm.derivatives                 = "automatic";
